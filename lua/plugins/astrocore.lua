9abc9553e8be8aea1ee97813758a9bd7ca997be9
Zi Makki <zimakki@gmail.com>
2025-04-05 13:56:34 +0200

    set scrolloff to 10

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -43,6 +43,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        scrolloff = 10, -- sets vim.opt.scrolloff
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
