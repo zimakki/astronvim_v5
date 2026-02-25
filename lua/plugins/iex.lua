caefac7729c4cfa2cc04c3a800c58b26860f1f4a
Zi Makki <zimakki@gmail.com>
2026-02-25 12:28:10 +0100

    Add iex.nvim plugin for IEx scratch file workflow

---
+++ lua/plugins/iex.lua
@@ -0,0 +1,9 @@
+return {
+  "jesseleite/iex.nvim",
+  ft = { "elixir" },
+  opts = {},
+  keys = {
+    { "<Leader>mi", "<cmd>IEx<cr>", desc = "Open IEx scratch file" },
+    { "<Leader>mr", "<cmd>IExRun<cr>", desc = "Run IEx scratch file" },
+  },
+}
